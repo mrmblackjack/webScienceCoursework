5c72e248539fc20fba6992e6bb973a55084f5798
mrmblackjack <anakimkn@abv.bg>
2020-03-20 16:36:49 +0000

    Update twitterAuth.py

--- twitterAuth.py
+++ twitterAuth.py
@@ -1,6 +1,6 @@
 
-consumer_key = "rycPsruCH3pYYL5KwoPOfJBS3"
-consumer_secret = "od1K6D1asmi4YPZumPZGmMt1y9ruTz6JY7q3xcdlYZM3Srjpak"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "564583421-c3Gfes3VqCVPTGGQOcyTumSQ9Jlulvv2S0gANQPR"
-access_token_secret = "AVtV9lyj35OmZFKlbu6IJmerb7TTgZyHaVOKdzkQTPW31"
+access_token = ""
+access_token_secret = ""
